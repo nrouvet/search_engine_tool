290076663b4be762bb1bbb8f9d382d6410cd26a2
emylz <eminyalaz@gmail.com>
2019-11-05 09:16:14 -0500

    crawler part1

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -4,6 +4,8 @@ import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
+import scala.io.Source
+
 object main extends App {
 
   val conf = new SparkConf().setAppName("BDD2").setMaster("local[*]")
@@ -34,5 +36,22 @@ object main extends App {
 
   //println(result.collect()(0)._1)
   result.collect().foreach(println)
+
+  val regexHref = """<li><a href=".*"""".r
+
+  val url = "http://legacy.aonprd.com/bestiary/"
+  val index = "monsterIndex.html"
+
+  val text = Source.fromURL(url+index)
+
+  val htmlPage = text.mkString
+
+  var listURL = regexHref.findAllIn(htmlPage).toList
+
+  println(listURL)
+
+  listURL(0)
+
+
 
 }
