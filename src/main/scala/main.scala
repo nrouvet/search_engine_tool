0ff495cd7d069dc6bf90a604c3bc34f7f12ac8e9
emylz <eminyalaz@gmail.com>
2019-11-12 08:16:43 -0500

    comments

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -9,33 +9,32 @@ import scala.util.matching.Regex
 
 object main extends App {
 
-  val url = "http://legacy.aonprd.com/bestiary/"
-  val index = "monsterIndex.html"
+  val url = "http://legacy.aonprd.com/bestiary/"  //link of the bestiary
+  val index = "monsterIndex.html"   //part of the link to get the index of the monsters
 
   val regexHref = """<li><a href=".*"""".r
-  val regexSpell = new Regex("""<a[^>]*\/spells\/[^>]*\#[^>]*>(\w+[^<]+)<\/a>""")
+  val regexSpell = new Regex("""<a[^>]*\/spells\/[^>]*\#[^>]*>(\w+[^<]+)<\/a>""")  //Regex to get the spells
   val regexName ="""href="(\w+[a-z]*)""".r
 
   val text = Source.fromURL(url+index)
+  val htmlPage = text.mkString    //Convert the HTML code of the page to a string
+  var listURL = regexHref.findAllIn(htmlPage).toList    //Find all the href link of the page with the regex and make a list with the result
 
-  val htmlPage = text.mkString
+  var monster = Array.empty[(String, Array[String])] //To store the monsters and theirs sorts
 
-  var listURL = regexHref.findAllIn(htmlPage).toList
-
-  var monster = Array.empty[(String, Array[String])]
-
-  for(i <- 0 until /*listURL.length*/1){
-    val nameMonster = regexName.findFirstMatchIn(listURL(i)).get.group(1)
-    val arrayUrl = listURL(i).split("\"")
+  //We will browse all the links we get previously to get all the sorts of all the monsters
+  for(i <- 0 until listURL.length){
+    val nameMonster = regexName.findFirstMatchIn(listURL(i)).get.group(1)   //We have a create a group with the regex in the brackets, we get the result with "get.group(1)
+    val arrayUrl = listURL(i).split("\"")  //Here we get the value of the link without the href or the quotation marks
     val getMonster = arrayUrl(1)
-    val text2 = Source.fromURL(url+getMonster).mkString
+    val text2 = Source.fromURL(url+getMonster).mkString  //We get the HTLM code of the page of the current monster
     var listSpells = List[String]()
-    regexSpell.findAllIn(text2).matchData.foreach{
+    regexSpell.findAllIn(text2).matchData.foreach{  //We get all the sorts display on the page of the current monster with the regex
       m =>
-        if(!listSpells.contains( m.group(1)))   //Evite les doublons
+        if(!listSpells.contains( m.group(1)))   //Avoid duplication
           listSpells = listSpells:+ m.group(1)
     }
-    monster = monster :+ (nameMonster, listSpells.toArray)
+    monster = monster :+ (nameMonster, listSpells.toArray)  //We create our couple of monster and their sorts and we add if to our list monster
   }
 
   println()
@@ -54,15 +53,17 @@ object main extends App {
 
   val result = rdd.flatMap{
     case(monster, sorts)=>
-      sorts.map(sort => (sort, monster))
+      sorts.map(sort => (sort, monster))  //For all the sort of each monster, we create a couple of (sort,monster)
   }
-      .reduceByKey((a,b)=>(a+","+b))
-      .mapValues(_.split(",").toArray)
+      .reduceByKey((a,b)=>(a+","+b))    //Then we reduce by key, so we group the monster by sort to inverse the couple to (sort, list(monster))
+      .mapValues(_.split(",").toArray)  //We create an array with the string create by the reduceByKey
 
-
-
-  //println(result.collect()(0)._1)
-  result.collect().foreach(println)
+  //result.collect().foreach(println)
+  /*for(i<-0 until result.collect().length){
+    println(result.collect()(i)._1)
+    println(result.collect()(i)._2.foreach(print))
+    println()
+  }*/
 
 
 }
