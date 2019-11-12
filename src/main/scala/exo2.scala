b97b07737a8839032ebb655119e10b1953fc26ec
emylz <eminyalaz@gmail.com>
2019-11-12 11:37:51 -0500

    graph

--- src/main/scala/exo2.scala
+++ src/main/scala/exo2.scala
@@ -2,7 +2,9 @@ import org.apache.spark.graphx.{Edge, Graph, VertexId}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SparkSession
 
-case class node(var id : Int, var name : String, var equipe : String)
+import scala.collection.mutable.ArrayBuffer
+
+case class node(var name : String, var equipe : String)
 
 case class edge(var node1 : node, var node2: node, var distance : Int)
 
@@ -18,58 +20,39 @@ object exo2 extends App {
       .getOrCreate()
 
 
-    var worgs1 = new node(1,"Worgs Rider",  "B")
-    var warlord = new node(2,"Le Warlord", "B")
-    var barbare = new node(3,"Barbares Orc", "B")
+    var worgs1 = new node("Worgs Rider",  "B")
+    var warlord = new node("Le Warlord", "B")
+    var barbare = new node("Barbares Orc", "B")
 
-    var solar = new node(4,"Solar", "A")
+    var solar = new node("Solar", "A")
 
-    var graph = Array((worgs1, Array(2,3,4)), (warlord, Array(1,4)), (barbare, Array(1,4)), (solar, Array(1,2,3,4)))
+    var graph = Array((worgs1, Array(2,3,4)), (warlord, Array(1,4)), (barbare, Array(1,4)), (solar, Array(1,2,3)))
 
     var rddGraph = sc.makeRDD(graph)
 
-    var edges = Array.empty[edge]
+    var edges = ArrayBuffer.empty[edge]
 
     for(i <- 0 until graph.length){
         for(j<-0 until graph(i)._2.length){
             if (graph(graph(i)._2(j)-1)._1.equipe == graph(i)._1.equipe) {
-                var tmp = new edge(graph(i)._1, graph(graph(i)._2(j)-1)._1, 50)
-                edges = edges :+ tmp
+                edges += edge(graph(i)._1, graph(graph(i)._2(j) - 1)._1, 50)
             }
             else {
-                var tmp = new edge(graph(i)._1, graph(graph(i)._2(j)-1)._1, 110)
-                edges = edges :+ tmp
+                edges += edge(graph(i)._1, graph(graph(i)._2(j)-1)._1, 110)
             }
         }
     }
 
-/*
-    var rddEdges = rddGraph.map{
-        monster =>
-            monster._2.map {
-              value =>
-                      if (graph(value-1)._1.equipe == monster._1.equipe) {
-                          var tmp = new edge(monster._1, graph(value-1)._1, 50)
-                          tmp
-                      }
-                      else {
-                          var tmp = new edge(monster._1, graph(value-1)._1, 110)
-                          tmp
-                      }
-          }
-    }
-*/
+    for(i<-0 until edges.length) {
+        for(j<-i+1 until edges.length-1) {
+            if(edges(i).node1 == edges(j).node2 && edges(j).node1 == edges(i).node2){
+                edges -= edges(j)
+            }
+        }
+    } //Remove duplicates
+
     var rddEdges = sc.makeRDD(edges)
 
     print()
-
-    /*
-    val nodes = sc.parallelize(Array((1L, ("Solar")), (2L, ("Pinto", )),
-            (3L, ("Worgs Rider")), (4L, ("Le Warlord" ),(5L,(" Barbares Orc")))))
-
-
-    val edges = sc.parallelize(Array(Edge(1L,2L),Edge(3L,4L),Edge(4L,5L),Edge(3L,5L)))
-*/
-
 
 }
